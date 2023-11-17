e702b815605f4431929914b6a6950b99b88e4bfa
Carson Myers <carson@myers.se>
2023-11-18 00:22:38 +0100

    Setup basic backend service

--- backend/src/main.rs
+++ backend/src/main.rs
@@ -1,12 +1,24 @@
+use axum::body::{boxed, Body};
+use axum::http::{Response, StatusCode};
 use axum::{routing::get, Router};
-
-async fn hello_world() -> &'static str {
-    "Hello, world!"
-}
+use tower::ServiceExt;
+use tower_http::services::ServeDir;
 
 #[shuttle_runtime::main]
 async fn main() -> shuttle_axum::ShuttleAxum {
-    let router = Router::new().route("/", get(hello_world));
+    let api_router = Router::new();
+
+    let router = Router::new()
+        .nest("/api", api_router)
+        .fallback_service(get(|req| async move {
+            match ServeDir::new("frontend/dist").oneshot(req).await {
+                Ok(res) => res.map(boxed),
+                Err(err) => Response::builder()
+                    .status(StatusCode::NOT_FOUND)
+                    .body(boxed(Body::from(format!("error: {err}"))))
+                    .expect("error response"),
+            }
+        }));
 
     Ok(router.into())
 }
